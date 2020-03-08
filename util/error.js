87d2ec22e108bc45298ba7568cb50813011031f2
cxc-public <57521136+cxc-public@users.noreply.github.com>
2020-03-08 18:32:12 +0800

    fix

--- util/error.js
+++ util/error.js
@@ -2,7 +2,8 @@ module.exports = {
   getCtxMsg(ctx) {
     return `\nURL: ${ctx.origin + ctx.url}\nMETHOD: ${ctx.method}\nCTX_STATE: ${JSON.stringify(
       ctx.state
-    )}\nPOST_DATA: ${JSON.stringify(ctx.request.body)}`;
+    )}\nCLIENT_INFO: ${ctx.headers['client-info']}\nPOST_DATA: ${JSON.stringify(ctx.request.body)}`;
+
   },
   formatError(ctx, err, length) {
     let error = `${JSON.stringify(err, Object.getOwnPropertyNames(err)).replace(/\\n/g, '\n')}`;
